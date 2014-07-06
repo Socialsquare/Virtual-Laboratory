6d1d0edf6a30326bf4f2ddf7221ec4ef7e92631c
Simon Altschuler <simon@altschuler.dk>
2014-07-06 10:33:52 +0200

    add ion auth module

--- api/application/config/config.php
+++ api/application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'ZK5u1euqP7ji3X4pbGoaIshM2dIJZhiQ';
 
 /*
 |--------------------------------------------------------------------------
