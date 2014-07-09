9c64f0120c054a46042bd5d1d16e5e87cc872e59
Markus Faerevaag <markus@faerevaag.no>
2014-07-09 16:30:38 +0200

    grunt: wip connect server

--- app/Gruntfile.js
+++ app/Gruntfile.js
@@ -1,3 +1,10 @@
+var tplProcess = require('./build-tools/tpl-process'),
+    path = require('path'),
+    mountFolder = function (connect, dir) {
+        return connect.static(path.resolve(dir.toString()));
+    };
+
+
 module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
@@ -11,18 +18,22 @@ module.exports = function (grunt) {
         copy: {
             dist: {
                 files: [
-                    // index
-                    { expand: true, src: [ 'index.html' ], dest: '<%= dist_root %>' },
                     // js
                     { expand: true, src: [ 'js/**' ], dest: '<%= dist_root %>' },
-                    // templates
-                    { expand: true, src: [ 'templates/**' ], dest: '<%= dist_root %>' },
+                    // views
+                    { expand: true, src: [ 'view/**' ], dest: '<%= dist_root %>' },
+                    // index
+                    { expand: true, src: [ 'index.html' ], dest: '<%= dist_root %>',
+                      options: {
+                          process: tplProcess.process(grunt, '<%= dist_root %>')
+                      } },
                     // images
                     { expand: true, src: [ 'img/**' ], dest: '<%= dist_root %>' },
                     // videos
                     { expand: true, src: [ 'videos/**' ], dest: '<%= dist_root %>' },
                     // vendor
-                    { expand: true, flatten: true, cwd: 'bower_components', src: [ '**/*.js' ], dest: '<%= dist_root %>/vendor' }
+                    { expand: true, flatten: true, cwd: 'bower_components',
+                      src: [ '**/*.js' ], dest: '<%= dist_root %>/vendor' }
                 ]
             }
         },
@@ -39,7 +50,31 @@ module.exports = function (grunt) {
         },
 
         jshint: {
-            all: [ 'Gruntfile.js', 'js/**/*.js', '!js/libs/**/*' ]
+            dist: [ 'Gruntfile.js', 'js/**/*.js', '!js/libs/**/*' ],
+        },
+
+        watch: {
+            dist: {
+                files: [ 'js/**/*.js', '!<%= dist_root %>/**' ],
+                tasks: [ 'build' ]
+            }
+        },
+
+        // TODO: livereload
+        connect: {
+            dist: {
+                options: {
+                    port: 8000,
+                    base: '<%= dist_root %>',
+                    middleware: function (connect, options) {
+                        return [
+                            tplProcess.middleware(grunt, '<%= dist_root %>'), // TODO: fix
+                            mountFolder(connect, '.tmp'),
+                            mountFolder(connect, options.base)
+                        ];
+                    }
+                }
+            }
         }
     });
 
@@ -47,8 +82,11 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
 
+    // TODO: enable jshint when smellz is cleaned
     grunt.registerTask('build', [ 'clean:dist', 'copy:dist', 'sass:dist' ]);
 
-    grunt.registerTask('default', [ 'jshint', 'build' ]);
+    grunt.registerTask('default', [ 'build', 'connect:dist', 'watch:dist' ]);
 };
---
+++ app/build-tools/tpl-process.js
@@ -0,0 +1,51 @@
+var path = require('path');
+
+var process = function (grunt, cwd) {
+    var tpls = '',
+        wrap,
+        files,
+        index,
+        model;
+
+    wrap = function (name, content) {
+        return '<script type="text/html" id="' + name + '">' +
+            content/*.replace(/\s+/gi, ' ')*/ +
+            '</script>';
+    };
+
+    files = grunt.file.expand(cwd + '/**/*.ko');
+
+    files.forEach(function (file) {
+        var name = path.basename(file, '.ko'),
+            content = grunt.file.read(file);
+
+        tpls += wrap(name, content);
+    });
+
+    index = grunt.file.read(cwd + '/index.html');
+    model = {
+        data: {
+            tpls: tpls
+        }
+    };
+
+    index = grunt.template.process(index, model);
+
+    grunt.log.writeln('Processed ' + files.length.toString().cyan + ' templates');
+
+    return index;
+}
+
+var middleware = function (grunt, path) {
+    return function (req, res, next) {
+        if (/^\/[^\/\.]*$/.test(req.originalUrl)) {
+            req.indexHtmlContentHack = process(grunt, path);
+        }
+        next();
+    };
+}
+
+module.exports = {
+    middleware: middleware,
+    process: process
+};
