b8d716536da2f3ee40cc9d873d0b3fd61da6aef8
Markus Faerevaag <markus@faerevaag.no>
2014-07-09 13:32:24 +0200

    bower: Cleaned up libs

--- app/js/main.js
+++ app/js/main.js
@@ -6,40 +6,24 @@
 var requireConfig = {
 	paths: {
 		cordova: '../cordova',
-		text: 'libs/requirejs/text',
-		jquery: 'libs/jquery/jquery-2.0.3.min',
-		underscore: 'libs/underscore.js/underscore-min',
-		lodash: 'libs/lodash.compat',
-		knockout: 'libs/knockout.js/knockout-3.1.0',
-		mapping: 'libs/knockout.mapping',
+		jquery: 'vendor/jquery',
+		lodash: 'vendor/lodash.compat',
+		knockout: 'vendor/knockout',
+		mapping: 'vendor/knockout.mapping',
 		base: 'libs/Base',
 
-		chcdraggable: 'libs/jquery/plugins/jquery.chcdraggable',
-		chcdraggablespawner: 'libs/jquery/plugins/jquery.chcdraggablespawner',
-		chcdroppable: 'libs/jquery/plugins/jquery.chcdroppable',
-		flot: 'libs/jquery/plugins/jquery.flot.min'
+		chcdraggable: 'libs/jquery/jquery.chcdraggable',
+		chcdraggablespawner: 'libs/jquery/jquery.chcdraggablespawner',
+		chcdroppable: 'libs/jquery/jquery.chcdroppable',
+		flot: 'libs/jquery/jquery.flot.min'
 	},
 	shim: {
 		'cordova': {
 			exports: 'cordova'
 		},
+
 		'jquery': {
 			exports: '$'
-		},
-		'underscore': {
-			deps: ['jquery'],
-			exports: '_',
-			init: function() {
-				//Tell Underscore template engine to use {{...}} for inserting values
-				this._.templateSettings = {
-					interpolate: /\{\{(.+?)\}\}/g
-				};
-				return _;
-			}
-		},
-		'backbone': {
-			deps: ['underscore'],
-			exports: 'Backbone'
 		},
 
 		'base': {
