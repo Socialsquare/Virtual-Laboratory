9da22271b5e418fc0ab42941f41f00cdb822b3f1
Markus Faerevaag <markus@faerevaag.no>
2014-07-11 16:05:55 +0200

    Cleaned requirejs config and added jquery.flot with bower

--- app/js/main.js
+++ app/js/main.js
@@ -1,71 +1,42 @@
-/**
- * appOriginals app bootstrap.
- * @author: Chris Hjorth, www.chrishjorth.com
- */
+require.config({
+    paths: {
+        jquery: '../bower_components/jquery/dist/jquery',
+        jqueryui: '../bower_components/jquery-ui/jquery-ui',
+        flot: '../bower_components/jquery-flot/jquery.flot',
+        lodash: '../bower_components/lodash/dist/lodash.compat',
+        knockout: '../bower_components/knockout/dist/knockout.debug',
+        mapping: '../bower_components/knockout-mapping/knockout.mapping',
 
-var requireConfig = {
-	paths: {
-		cordova: '../cordova',
-		jquery: '../bower_components/jquery/dist/jquery',
-		jqueryui: '../bower_components/jquery-ui/jquery-ui',
-		lodash: '../bower_components/lodash/dist/lodash.compat',
-		knockout: '../bower_components/knockout/dist/knockout.debug',
-		mapping: '../bower_components/knockout-mapping/knockout.mapping',
-		base: 'libs/Base',
+        base: 'libs/Base'
+    },
+    shim: {
+        'jqueryui': ['jquery'],
+        'flot': ['jquery'],
 
-		flot: 'libs/jquery/jquery.flot.min'
-	},
-	shim: {
-		'cordova': {
-			exports: 'cordova'
-		},
-
-		'jquery': {
-			exports: '$'
-		},
-
-		'jqueryui': {
-			deps: ['jquery']
-		},
-
-		'base': {
-			exports: 'Base'
-		},
-
-		'knockout': {
-			deps: ['jquery'],
-			exports: 'ko'
-		},
-
-		'flot': {
-			deps: ['jquery'],
-			exports: 'flot'
-		}
-	}
-};
-
-require.config(requireConfig);
+        'base': { exports: 'Base' }
+    }
+});
 
 require([
     'knockout',
-	'controller/App',
+    'controller/App',
 
     // self-registering jquery plugins
-	'jqueryui',
-	'flot',
+    'jqueryui',
+    'flot',
 
-    // register knockout bindings and extensions
+    // self-registering knockout bindings and extensions
     'bindings/dragging',
     'bindings/video',
     'bindings/plotting',
     'bindings/routing',
     'extensions/extensions'
 ], function(ko, App) {
-	var isWeb = document.URL.match(/^https?:/);
+    var isWeb = document.URL.match(/^https?:/);
 
     var app = new App(isWeb);
 
     var appElement = document.getElementById('#app');
 
-	ko.applyBindings(app, appElement);
+    ko.applyBindings(app, appElement);
 });
