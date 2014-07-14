faaba1fd8af16e29c5624bd59f3bb6f49b65071f
Markus Faerevaag <mafaer@gmail.com>
2014-07-14 18:51:10 +0200

    Fixed ContainerType values

--- app/js/model/type/Container.js
+++ app/js/model/type/Container.js
@@ -1,26 +1,26 @@
 define([], function () {
     return {
         // Simple
-        PETRI_DISH:        'ContainType.PETRI_DISH',
-        MICROTITER:        'ContainType.MICROTITER',
-        TUBE:              'ContainType.TUBE',
-        FERMENTOR_TANK:    'ContainType.FERMENTOR_TANK',
-        BOTTLE:            'ContainType.BOTTLE',
-        SYRINGE:           'ContainType.SYRINGE',
+        PETRI_DISH:        'ContainerType.PETRI_DISH',
+        MICROTITER:        'ContainerType.MICROTITER',
+        TUBE:              'ContainerType.TUBE',
+        FERMENTOR_TANK:    'ContainerType.FERMENTOR_TANK',
+        BOTTLE:            'ContainerType.BOTTLE',
+        SYRINGE:           'ContainerType.SYRINGE',
 
         // Composite
-        PETRI_SPACE:       'ContainType.PETRI_SPACE',
-        MICRO_SPACE:       'ContainType.MICRO_SPACE',
-        TUBE_RACK:         'ContainType.TUBE_RACK',
-        UV:                'ContainType.UV',
+        PETRI_SPACE:       'ContainerType.PETRI_SPACE',
+        MICRO_SPACE:       'ContainerType.MICRO_SPACE',
+        TUBE_RACK:         'ContainerType.TUBE_RACK',
+        UV:                'ContainerType.UV',
 
         // Simple machines
-        ELECTROPORATOR:    'ContainType.ELECTROPORATOR',
-        BLENDER:           'ContainType.BLENDER',
+        ELECTROPORATOR:    'ContainerType.ELECTROPORATOR',
+        BLENDER:           'ContainerType.BLENDER',
 
         // Composite machines
-        HEATER:            'ContainType.HEATER',
-        OD_MACHINE:        'ContainType.OD_MACHINE',
-        SPECTROPM_MACHINE: 'ContainType.SPECTROPM_MACHINE'
+        HEATER:            'ContainerType.HEATER',
+        OD_MACHINE:        'ContainerType.OD_MACHINE',
+        SPECTROPM_MACHINE: 'ContainerType.SPECTROPM_MACHINE'
     };
 });
