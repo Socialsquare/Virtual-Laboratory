b6a37c51fdc8a3fd7f92b7e9745b4bbd108a12d2
Simon Altschuler <simon@altschuler.dk>
2014-07-18 11:52:44 +0200

    better sequencing computer screen

--- app/js/controller/view/computer/Sequencing.js
+++ app/js/controller/view/computer/Sequencing.js
@@ -16,38 +16,60 @@ define([
             var self = this;
             self.base('computer-sequencing', 'computer.screen.sequencing');
 
+            self.isValid = ko.observable(false);
             self.message = ko.observable('');
+            self.item = null;
+            self.consumeItem = null;
 
-            self.handleDrop = function (item) {
+            self.handleDrop = function (item, consumer) {
                 if (item.getTotalConcentration() >= 10) {
+                    self.isValid(false);
                     self.message('computer.screen.sequencing.invalid_concentration');
                     return false;
                 }
 
                 if (!item.containsMicroorganism(MicroorganismType.MYELOMA)) {
+                    self.isValid(false);
                     self.message('computer.screen.sequencing.missing_myeloma');
                     return false;
                 }
 
                 if (!item.contains(LiquidType.ANTIBODY_GOUT) && !item.contains(LiquidType.ANTIBODY_SMALLPOX)) {
+                    self.isValid(false);
                     self.message('computer.screen.sequencing.missing_antibody');
                     return false;
                 }
 
+                if (item.contains(LiquidType.ANTIBODY_GOUT))
+                    self.message('computer.screen.sequencing.valid.gout');
+                else if (item.contains(LiquidType.ANTIBODY_SMALLPOX))
+                    self.message('computer.screen.sequencing.valid.smallpox');
+
+                self.item = item;
+                self.consumeItem = consumer;
+                self.isValid(true);
+
+                return false;
+            };
+
+            self.sendToSequencing = function () {
+                if (!self.item) return;
                 // reset message and create new dna element
                 var dna = null;
-                if (item.contains(LiquidType.ANTIBODY_GOUT)) {
+                if (self.item.contains(LiquidType.ANTIBODY_GOUT)) {
                     dna = self.createDNAElement(LiquidType.ANTIBODY_GOUT);
                     popupController.message('computer.screen.sequencing.created_gout.header',
                                             'computer.screen.sequencing.created_gout.body');
                 }
-                else if (item.contains(LiquidType.ANTIBODY_SMALLPOX)) {
+                else if (self.item.contains(LiquidType.ANTIBODY_SMALLPOX)) {
                     dna = self.createDNAElement(LiquidType.ANTIBODY_SMALLPOX);
                     popupController.message('computer.screen.sequencing.created_smallpox.header',
                                             'computer.screen.sequencing.created_smallpox.body');
                 }
+
                 // TODO: maybe check if it exists already?
                 self.gameState.sequencedDNA.push(dna);
+                self.consumeItem();
                 self.message('');
             };
 
