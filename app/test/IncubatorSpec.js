57c6edddf76b47a84a09313981b2492e89645130
patrick <patrick_gadd@hotmail.com>
2014-07-19 11:43:34 +0200

    Testing of the incubator

--- app/test/IncubatorSpec.js
+++ app/test/IncubatorSpec.js
@@ -1,11 +1,14 @@
 define([
+    'utils/utils',
     'factory/Container',
     'factory/Liquid',
     'model/Incubator',
+    'model/Pipette',
+    'model/Tip',
     'model/type/Liquid',
     'model/type/Microorganism',
     'extensions/extensions' //TODO: have this as the _last_ requirement
-], function (CF, LF, IncubatorModel, LiquidType, MicroorganismType) {
+], function (utils, CF, LF, IncubatorModel, PipetteModel, TipModel, LiquidType, MicroorganismType) {
 
     describe('Simple container', function() {
         var tubeYeast, tube;
@@ -27,6 +30,49 @@ define([
             expect(concBefore < incubator.tubeRack.get(1).getTotalConcentration()).toBeTruthy();
             expect(concBefore < tubeYeast.getTotalConcentration()).toBeTruthy();
         });
+
+
+        it('Grow orignal and klone', function () {
+            var kloned = utils.klone(tubeYeast);
+            incubator.tubeRack.addAt(1, tubeYeast);
+            incubator.tubeRack.addAt(3, kloned);
+
+            var concBefore = tubeYeast.getTotalConcentration();
+            var concBeforeKlone = kloned.getTotalConcentration();
+
+            incubator.growOneHour();
+            console.log('Total conc: ' + tubeYeast.getTotalConcentration());
+
+            expect(concBefore < incubator.tubeRack.get(1).getTotalConcentration()).toBeTruthy();
+            expect(concBefore < tubeYeast.getTotalConcentration()).toBeTruthy();
+            expect(concBeforeKlone < incubator.tubeRack.get(3).getTotalConcentration()).toBeTruthy();
+            expect(concBeforeKlone < kloned.getTotalConcentration()).toBeTruthy();
+        });
+
+        it('Grow orignal and klone by pipette', function () {
+            console.log('Grow original and klone by pipette');
+            var pipette = new PipetteModel();
+            console.log('yo dawg');
+            pipette.addAt(0, new TipModel());
+
+            pipette.fillPipette(tubeYeast);
+            pipette.emptyPipetteInto(tube);
+
+            incubator.tubeRack.addAt(1, tubeYeast); //TODO: fill pipette after placing. Just to be sure!
+            incubator.tubeRack.addAt(3, tube);
+
+            var concBefore = tubeYeast.getTotalConcentration();
+            var concBeforeKlone = tube.getTotalConcentration();
+
+            incubator.growOneHour();
+            console.log('Total conc: ' + tubeYeast.getTotalConcentration());
+
+            expect(concBefore < incubator.tubeRack.get(1).getTotalConcentration()).toBeTruthy();
+            expect(concBefore < tubeYeast.getTotalConcentration()).toBeTruthy();
+            expect(concBeforeKlone < incubator.tubeRack.get(3).getTotalConcentration()).toBeTruthy();
+            expect(concBeforeKlone < tube.getTotalConcentration()).toBeTruthy();
+        });
+
 
         it('Do not grow much beyond limit', function () {
             incubator.tubeRack.addAt(1, tubeYeast);
