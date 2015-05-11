27ab98b0f13ac154251a6643521c66a9dced582c
Simon Altschuler <simon@altschuler.dk>
2015-05-11 15:36:59 +0200

    add buffy coat model with blood type

---
+++ app/js/model/BuffyCoat.ts
@@ -0,0 +1,30 @@
+import ko = require('knockout');
+
+import LiquidModel = require('model/Liquid');
+import ReactionCount = require('model/ReactionCount');
+
+import LiquidType = require('model/type/Liquid');
+import MouseBloodType = require('model/type/MouseBlood');
+
+class BuffyCoat extends LiquidModel {
+
+    public bloodType: KnockoutObservable<MouseBloodType>;
+
+    constructor(bloodType: MouseBloodType) {
+        super(LiquidType.BUFFY_COAT, ReactionCount.NEVER, true);
+
+		this.bloodType = ko.observable(bloodType);
+
+        ko.rebind(this);
+    }
+
+    hashCode() {
+        return this._hashCode() + ":" + this.bloodType();
+    }
+
+    clone() {
+        return new BuffyCoat(this.bloodType());
+    }
+}
+
+export = BuffyCoat;
