c68de9ceb533b4930cf485b8f15d1d58075dc7ee
pawciobiel <pawciobiel@gmail.com>
2016-01-26 18:08:03 +0100

    fix utils.pickRandomValue and update tests

--- app/test/UtilsSpec.ts
+++ app/test/UtilsSpec.ts
@@ -1,6 +1,8 @@
+$ = require('jquery');
 import LiquidFactory = require('factory/Liquid');
 import ContainerFactory = require('factory/Container');
 import Utils = require('utils/utils');
+
 
 describe('Dilution', () => {
     it('should dilute liquid', () => {
@@ -18,5 +20,29 @@ describe('Dilution', () => {
         tube.liquids(diluted);
 
         expect(tube.getTotalConcentration()).toBe(conc / factor);
+    });
+});
+
+describe('pickRandomValue', () => {
+    it('should return random value from an array', () => {
+        var someValsArr = [1, 2, 3, 4, 5];
+        var actual = Utils.math.pickRandomValue(someValsArr);
+        var expVal = $.inArray(actual, someValsArr); 
+        expect(expVal > -1).toBe(true);
+    });
+    it('should return undefined from an empty array', () => {
+        var someValsArr = [];
+        var actual = Utils.math.pickRandomValue(someValsArr);
+        expect(actual === undefined).toBe(true);
+    });
+    it('should return the value from one item array', () => {
+        var someValsArr = [13];
+        var actual = Utils.math.pickRandomValue(someValsArr);
+        expect(actual === 13).toBe(true);
+    });
+    it('should throw an exception if first argument is not an Array', () => {
+        var someVals = {spam: "eggs"};
+        var expectedMsg = "TypeError: Utils.math.pickRandomValue takes Array as a first and only argument";
+        expect(function(){Utils.math.pickRandomValue(someVals);}).toThrow(new Error(expectedMsg));
     });
 });
