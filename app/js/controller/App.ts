d73c6e39c32412d32d87f7274880fc2d6966bb61
Kristoffer Olesen <kristofferdo@gmail.com>
2016-11-02 10:42:28 +0100

    [FIX] Removing the attempt at fixing image dragging, as it disabled other features

--- app/js/controller/App.ts
+++ app/js/controller/App.ts
@@ -41,12 +41,7 @@ class App extends BaseViewController {
     constructor(isWeb) {
         super('app');
 
-        // Prevent move images default
-        document.onmousedown = function(e) {
-            if(e.target.tagName === 'IMG') {
-                e.preventDefault()
-            }
-        }
+
 
         this.activeViewController = ko.observable(null);
 
