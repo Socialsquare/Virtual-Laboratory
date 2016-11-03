7fd7fe8885f0cba4fa839f055c399df37979b277
Andreas Larsen <andreas@larsenwork.com>
2016-11-03 20:49:12 +0100

    [ADD] also preload jpg

--- app/build-tools/assets.js
+++ app/build-tools/assets.js
@@ -24,6 +24,7 @@ function size(file) {
 module.exports = {
     generate: function (root, assetsDir, callback) {
         var assetsConfig = { files: [] };
+        var imageFormats = ['/**/*.png', '/**/*.jpg'];
         var distPath = '';
 
         _.each(glob.sync(root + '/' + assetsDir + '/**/*.mp4'), function (file) {
@@ -31,9 +32,11 @@ module.exports = {
             assetsConfig.files.push(makeVideoEntry(distPath, size(file)));
         });
 
-        _.each(glob.sync(root + '/' + assetsDir + '/**/*.png'), function (file) {
-            distPath = path.relative(root, file).replace('app/','');
-            assetsConfig.files.push(makeImageEntry(distPath, size(file)));
+        imageFormats.forEach(function(format) {
+          _.each(glob.sync(root + '/' + assetsDir + format), function (file) {
+              distPath = path.relative(root, file).replace('app/','');
+              assetsConfig.files.push(makeImageEntry(distPath, size(file)));
+          });
         });
 
         return assetsConfig;
